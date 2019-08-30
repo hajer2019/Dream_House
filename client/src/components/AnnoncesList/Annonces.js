461ae54ba8d8803f0f137bc1734686a6a5814fa6
wael <waelattafi1@gmail.com>
2019-08-30 16:36:00 +0100

    annonce list

--- client/src/components/AnnoncesList/Annonces.js
+++ client/src/components/AnnoncesList/Annonces.js
@@ -5,11 +5,6 @@ import { connect } from "react-redux";
 import AnnonceInfo from "./AnnonceInfo";
 import image1 from "../../images/img1.jpg";
 
-
-import AnnonceInfo from "./AnnonceInfo";
-import image1 from "../../images/img1.jpg";
-
-
 class AnnoncesList extends Component {
   constructor(props) {
     super(props);
